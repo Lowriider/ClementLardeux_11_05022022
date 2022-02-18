9430ff983c4eb1ab8cc2093036d9efac52625cda
Clément <lowriider23@gmail.com>
2022-02-18 22:13:08 +0100

    :sparkles: add route stand-alone file

--- src/Routage/Router.js
+++ src/Routage/Router.js
@@ -1,19 +1,28 @@
-// import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
-// import Home from "../js/Pages/Home";
-// import ErrorPage from "../js/Pages/404";
-//
-// const Router = () => {
-//     return (
-//         <BrowserRouter>
-//             <Routes>
-//                 <Route path="/" element={<Home/>}/>
-//                 <Route path="/about" element={<About/>}/>
-//                 <Route path="/about" element={<Article/>}/>
-//                 <Route path="*" element={<Navigate to={<ErrorPage/>}/>}
-//                     />
-//             </Routes>
-//         </BrowserRouter>
-//     )
-// }
-//
-// export default Router
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import Home from "../../src/js/Pages/Home";
+import Article from "../../src/js/Pages/Article";
+import About from "../../src/js/Pages/About";
+import NotFound from "../../src/js/Pages/NotFound"
+
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="about" element={<About/>}/>
+            <Route path="/article/:key/:rentId" element={<Article/>}/>
+            <Route
+                path="*"
+                element={<NotFound/>}
+            />
+        </Routes>
+    </BrowserRouter>
+    )
+}
+
+export default Router
--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from "react-router-dom";
-import Home from "./js/Pages/Home";
-import Article from "./js/Pages/Article";
-import About from "./js/Pages/About";
-import NotFound from "./js/Pages/NotFound"
+import Router from "./Routage/Router";
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="about" element={<About/>}/>
-            <Route path="/article/:key/:rentId" element={<Article/>}/>
-            <Route
-                path="*"
-                element={<NotFound/>}
-            />
-        </Routes>
-    </BrowserRouter>,
+    <Router/>,
     document.getElementById("root")
 );
 
