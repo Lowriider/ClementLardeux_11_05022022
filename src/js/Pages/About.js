7de4b2e67fa9e4adcbeb057bda4fda0d32805351
Clément <lowriider23@gmail.com>
2022-02-18 18:59:31 +0100

    :recylce: refacto about dropdown

--- src/js/Pages/About.js
+++ src/js/Pages/About.js
@@ -6,10 +6,22 @@ import Dropdown from "../Components/Dropdown";
 const About = () => {
 
     const listDropdown = [
-        'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.',
-        'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
-        'Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N\'hésitez pas à nous contacter si vous avez la moindre question.',
-        'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l\'hôte qu\'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.',
+        {
+            value: 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.',
+            title: 'Fiabilité'
+        },
+        {
+            value: 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+            title: 'Respect'
+        },
+        {
+            value: 'Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N\'hésitez pas à nous contacter si vous avez la moindre question.',
+            title: 'Sécurité'
+        },
+        {
+            value:'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l\'hôte qu\'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.',
+            title: 'Service'
+        },
     ]
 
     return (
@@ -20,20 +32,17 @@ const About = () => {
                 </div>
             </div>
             <div className="about-container">
-                    <div className="about-dropdown">
-                        <Dropdown datas={listDropdown[0]} title={"Fiabilité"}/>
-                    </div>
-                    <div className="about-dropdown">
-                        <Dropdown datas={listDropdown[1]} title={"Respect"}/>
-                    </div>
-                    <div className="about-dropdown">
-                        <Dropdown datas={listDropdown[2]} title={"Service"}/>
-                    </div>
-                    <div className="about-dropdown">
-                        <Dropdown datas={listDropdown[3]} title={"Sécurité"}/>
-                    </div>
+                        {
+                            listDropdown.map(dropdown => {
+                                return (
+                                    <div className="about-dropdown">
+                                        <Dropdown datas={dropdown.value} title={dropdown.title}/>
+                                    </div>
+                                )
+                            })
+                        }
             </div>
-            {/*<MainFooter/>*/}
+            <MainFooter/>
         </div>
     )
 }
